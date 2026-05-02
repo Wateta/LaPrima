ade6f3411d57d5fc1853fda4ed8a4d88db58f71f
Wateta <shimirwasonia5@gmail.com>
2026-05-02 13:22:33 +0200

    Initial commit

--- LaPrima_Frontend/src/pages/landingPage.jsx
+++ LaPrima_Frontend/src/pages/landingPage.jsx
@@ -13,7 +13,7 @@ function LandingPage() {
                     <p className="landing-page-subtitle"> THE COFFEE SHOP PLATFORM</p>
                     </div>
                      <h1 className="landing-page-title">Where Every </h1>
-                    <h1 className="landing-page-subtitle">Coffee Shop</h1>
+                    <h1 className="landing-page-subtitle-coffee">Coffee Shop</h1>
                     <h1 className="landing-page-subtitle-secondary">Thrives Online</h1>
                     <p className="landing-page-description">
                         La Prima connects coffee shop owners with their customers. Register your shop, manage your menu, track orders, and grow your community — all in one elegant platform.
