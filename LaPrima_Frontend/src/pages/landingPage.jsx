3cb47aa8543d52512430623f5fb8a1757b9a299e
Wateta <shimirwasonia5@gmail.com>
2026-05-02 21:19:56 +0200

    Initial commit

--- LaPrima_Frontend/src/pages/landingPage.jsx
+++ LaPrima_Frontend/src/pages/landingPage.jsx
@@ -4,7 +4,7 @@ function LandingPage() {
     return (
         <div className="landing-page">
             <div className="landing-navbar">
-                <div className="navbar-logo">a Prima</div>
+                <div className="navbar-logo"><span className='la'>la</span> Prima</div>
                 <div className="navbar-links">
                     <a href="#about">About</a>
                     <a href="#how-it-works">How it works</a>
