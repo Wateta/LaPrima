fba51146fdd66a1f3eb2ad7355c86f71932a38c5
Wateta <shimirwasonia5@gmail.com>
2026-05-02 11:57:45 +0200

    Initial commit

--- LaPrima_Frontend/src/pages/landingPage.jsx
+++ LaPrima_Frontend/src/pages/landingPage.jsx
@@ -1,28 +1,32 @@
 import './landingPage.css'
-function LandingPage(){
+
+function LandingPage() {
     return (
         <div className="landing-page">
-           <div className="landing-navbar">
+            <div className="landing-navbar">
 
-           </div>
-           <div className="landing-content">
-            <div className="left">
-         <h1 className="landing-page-title">THE COFFEE SHOP PLATFORM</h1>
-         <h2 className="landing-page-subtitle">Where Every Coffee </h2>
-         <h2 className="landing-page-subtitle">Coffee Shop</h2>
-          <h2 className="landing-page-subtitle">Thrives Online</h2>
-          <p className="landing-page-description">
-           La Prima connects coffee shop owners with their customers. Register your shop, manage your menu, track orders, and grow your community — all in one elegant platform.
-          </p>
-          <div className="landing-buttons">
-          <button className="landing-register">Register your shop</button>
-          <button className="landing-register">See how it works</button>
-          </div>
-          </div>
-          <div className="right">
+            </div>
+            <div className="landing-content">
+                <div className="left">
+                   
+                    <h2 className="landing-page-subtitle"> THE COFFEE SHOP PLATFORM</h2>
+                     <h1 className="landing-page-title">Where Every Coffee</h1>
+                    <h2 className="landing-page-subtitle">Coffee Shop</h2>
+                    <h2 className="landing-page-subtitle">Thrives Online</h2>
+                    <p className="landing-page-description">
+                        La Prima connects coffee shop owners with their customers. Register your shop, manage your menu, track orders, and grow your community — all in one elegant platform.
+                    </p>
+                    <div className="landing-buttons">
+                        <button className="landing-register">Register your shop</button>
+                        <button className="landing-register">See how it works</button>
+                    </div>
+                </div>
+                <div className="right">
 
-          </div>
-           </div>
+                </div>
+            </div>
         </div>
     )
 }
+
+export default LandingPage
